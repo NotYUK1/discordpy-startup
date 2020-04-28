be953fab4308743eee7afb0fce6c124a3a9221c8
NotYUK1 <64450716+NotYUK1@users.noreply.github.com>
2020-04-28 23:22:12 +0900

    Add files via upload

--- setting.py
+++ setting.py
@@ -7,4 +7,5 @@ load_dotenv(verbose=True)
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-DTOKEN = os.environ.get("DISCORD_TOKEN")
+#DTOKEN = os.environ.get("DISCORD_TOKEN")
+DTOKEN = os.environ['DISCORD_BOT_TOKEN']
